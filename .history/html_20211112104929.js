f3aa33de980a1d36356ffe60eef9b1d83d30aec0
huyuqiong <huyuqiong@jwzg.com>
2021-11-12 11:08:39 +0800

    fix: update .gitignore

--- .history/html_20211112104929.js
+++
@@ -1,5 +0,0 @@
-/*
- * @Date: 2021-11-12 10:49:29
- * @Description:
- */
-module.exports = require('./dist/html');
--- .history/index_20211112105223.js
+++
@@ -1,5 +0,0 @@
-/*
- * @Date: 2021-11-12 10:49:34
- * @Description:
- */
-module.exports = require('./dist/webcomponent');
